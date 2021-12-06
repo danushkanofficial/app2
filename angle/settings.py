7c2e9fa829d0207099537cd9474bbcd651b37ac7
danushkanofficial <68120566+danush31@users.noreply.github.com>
2021-12-06 19:27:27 +0530

    d

--- angle/settings.py
+++ angle/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-dvcmwgtl=m4c^m0r*m8brna&_zh2e7b_47&yyd6$-f71_27+#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['danushkan03.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
