1710b122c014cd7e9c36d8d5b5095618aa90ae52
danushkanofficial <68120566+danush31@users.noreply.github.com>
2021-12-06 20:10:02 +0530

    cvbcb

--- angle/settings.py
+++ angle/settings.py
@@ -121,10 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join( BASE_DIR, 'static')
+STATIC_ROOT = os.path.join( BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    'angle/static',
+    os.path.join(BASE_DIR, 'angle/static')
 ]
+
 
 MEDIA_URL   =   '/media/'
 MEDIA_ROOT  =   BASE_DIR/'media'
