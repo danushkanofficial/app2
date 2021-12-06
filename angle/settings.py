963faad667f5ee31b4a1492ab8f114d995db524d
danushkanofficial <68120566+danush31@users.noreply.github.com>
2021-12-06 20:03:26 +0530

    xcvxcxcv

--- angle/settings.py
+++ angle/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = os.path.join( BASE_DIR, 'static')
 STATICFILES_DIRS = [
     'angle/static',
 ]
