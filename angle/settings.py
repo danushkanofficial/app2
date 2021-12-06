312988476d4f6284c4ca20e5fdb2f483a8cbb346
danushkanofficial <68120566+danush31@users.noreply.github.com>
2021-12-06 20:17:29 +0530

    cc

--- angle/settings.py
+++ angle/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'angle.wsgi.application'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
